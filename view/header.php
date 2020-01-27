7d75681300d8280f437b38b015d7bdbdc374c369
HBull5 <Harcourtbullv@gmail.com>
2020-01-26 21:19:36 -0500

    added CH 24 book-apps, very broken fixed some but not all

---
+++ view/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<!-- the head section -->
+<head>
+    <title>My Guitar Shop</title>
+    <link rel="stylesheet" type="text/css"
+          href="<?php echo $app_path ?>main.css">
+<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+
+<body>
+<header>
+    <h1>My Guitar Shop</h1>
+</header>
